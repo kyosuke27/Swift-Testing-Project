8c0ca1d1081e1bfb045271747d3a5769a7a40c06
Kyosuke Noguchi <kn1359028527@gmail.com>
2026-06-05 00:46:46 +0900

    initial commit

---
+++ Swift-Test-Project/Swift_Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Test_ProjectApp.swift
+//  Swift-Test-Project
+//
+//  Created by kyosuke on 2026/06/05.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
